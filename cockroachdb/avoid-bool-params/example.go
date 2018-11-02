b41d34319e0b8a73410710c38dcfb497c963f97d
daanikus <daanikus@gmail.com>
2018-11-02 16:45:13 +1300

    Update fmt-verbs and add avoid-bool-params tenet

---
+++ cockroachdb/avoid-bool-params/example.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    fmt.Println("Hello, world!")
+}
+
+func foo(bar bool) {
+    fmt.Println("Hello, foo!")
+}
+
+func far(boo bool, a int) {
+    fmt.Println("Hello, far!")
+}

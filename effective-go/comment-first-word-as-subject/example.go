4dbf37a5f18fdc58a2ce8e204b3eef3b059176cf
daanikus <daanikus@gmail.com>
2018-11-07 12:13:00 +1300

    Added EG tenet that checks first word in func comment == name of func

---
+++ effective-go/comment-first-word-as-subject/example.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    fmt.Println("Hello, playground")
+    // This comment not on a func decl 
+}
+
+/* foo is the first word
+*/
+func foo() {}
+
+/* This func comment should begin with 'bar'
+*/
+func bar() {}
